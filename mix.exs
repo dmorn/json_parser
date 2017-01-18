0ef26d55b914e8eda91e052114a92d0cfa28ef2c
Daniel Morandini <danielmorandini@me.com>
2017-01-18 11:30:25 +0100

    :bookmark: Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule JsonParser.Mixfile do
 
   def project do
     [app: :json_parser,
-     version: "1.0.0",
+     version: "1.0.5",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
